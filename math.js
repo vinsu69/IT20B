7df4df27b44fc173c911d20493f76e8ab31ba848
vinsu69 <mantejoewin@gmail.com>
2025-11-18 08:56:56 +0800

    math js done

---
+++ math.js
@@ -0,0 +1,73 @@
+// function legalAge(age) {
+    
+//     if (age >= 18) {
+//         console.log("You are an adult.");
+//     } else {
+//         console.log("You are not an adult.");
+//     }
+// }
+
+// legalAge(69);
+
+// function computerGrade(grade) {
+
+//     if (grade >= 94.5) {
+//         console.log("1");
+
+//     } else if (grade >= 91.5) {
+//         console.log("1.25");
+
+//     } else if (grade >= 89.5) {
+//         console.log("1.5");
+
+//     } else if (grade >= 86.5) {
+//         console.log("1.75");
+
+//     } else if (grade >= 84.5) {
+//         console.log("2");
+//     } else {
+//         console.log("Not within the transmutation.");
+//     }
+// }
+
+// computerGrade(87);
+
+// function login(username, password) {
+
+//     if(username === "makie"){
+        
+
+//         if(password === "ikawangpassword"){
+//             console.log("Welcome Makie!");
+
+//         } else {
+//             console.log("Invalid Login Credential.");
+//         }
+
+//         } else {
+//             console.log("Invalid User.");
+
+//     }
+// }
+
+// login("makie", "ikawangpassword");
+
+function ticketPrice(age) {
+    if (age < 18) {
+        if (age < 5) {
+            console.log("Ticket: Free");
+
+        } else {
+            console.log("Ticket: Child Price");
+        }
+    } else {
+        if (age >= 60) {
+            console.log("Ticket: Senior Senior Discount");
+        } else {
+            console.log("Ticket: Adult Price");
+
+        }
+    }
+}   
+
+ticketPrice(5);
