eef5b235bf15314e72475d567d57f03e2c4ab112
vinsu69 <mantejoewin@gmail.com>
2025-12-03 17:50:42 +0800

    Added insert at end method for DSA/linkedlist

--- DSA/DSA/DSA/linkedlist.js
+++ DSA/DSA/DSA/linkedlist.js
@@ -1,38 +1,57 @@
 class Node{
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+constructor(value){
+this.value = value;
+this.next = null;
+}
 }
 
 class LinkedList{
 
-    constructor(){
-        this.head = null;
-    }
+constructor(){
+this.head = null;
+}
 
-    insert_at_beginning(element){
+insert_at_beginning(element){
+const node = new Node(element);
+node.next = this.head;
+this.head = node;
+}
+
+traverse(){
+let current = this.head;
+let output = "";
+
+while(current){
+output += current.value + " ";
+current = current.next;
+}
+
+return output;
+}
+
+    insert_at_end(element){
         const node = new Node(element);
-        node.next = this.head;
-        this.head = node;
-    }
 
-    traverse(){
-        let current = this.head;
-        let output = "";
+        if(!this.head){
+            this.head = node;
+            return;
+        }
 
-        while(current){
-            output += current.value + " ";
+        let current  = this.head;
+
+        while(current.next){
             current = current.next;
         }
 
-        return output;
+        current.next = node;
     }
-
 }
 
 let list1 = new LinkedList();
 list1.insert_at_beginning(4);
 list1.insert_at_beginning(2);
 list1.insert_at_beginning(3);
+list1.insert_at_end(4);
+list1.insert_at_end(2);
+list1.insert_at_end(3);
 console.log(list1.traverse());
