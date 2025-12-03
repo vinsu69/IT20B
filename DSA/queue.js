c2b5044dd12d324ac2b04c81420aec7f0a359830
vinsu69 <mantejoewin@gmail.com>
2025-12-03 17:36:30 +0800

    Added utility methods for DSA/queue.js

--- DSA/queue.js
+++ DSA/queue.js
@@ -1,30 +1,49 @@
 class Queue{
 
-    constructor(){
-        this.data = {};
-        this.frontIndex = 0;
-        this.rearIndex = -1;
+constructor(){
+this.data = {};
+this.frontIndex = 0;
+this.rearIndex = -1;
+}
+
+enqueue(element){
+this.rearIndex++;
+this.data[this.rearIndex] = element;
+}
+
+traverse(){
+let output = "";
+for(let i=this.frontIndex; i <= this.rearIndex; i++){
+output += this.data[i] + " ";
+}
+
+return output;
+}
+
+    is_empty(){
+        return this.frontIndex > this.rearIndex;
     }
 
-    enqueue(element){
-        this.rearIndex++;
-        this.data[this.rearIndex] = element;
+    front(){
+        //Correction Statement
+        if(this.is_empty()) return null;
+        return this.data[this.frontIndex];
     }
 
-    traverse(){
-        let output = "";
-        for(let i=this.frontIndex; i <= this.rearIndex; i++){
-            output += this.data[i] + " ";
-        }
-
-        return output;
+    size(){
+        return this.rearIndex - this.frontIndex +1;
     }
-
 }
 
 //
 let queue1 = new Queue();
+console.log(queue1.is_empty());
 queue1.enqueue(4);
+console.log("The current queue size is: " + queue1.size());
 queue1.enqueue(2);
+console.log("The current queue size is: " + queue1.size());
 queue1.enqueue(3);
 console.log(queue1.traverse());
+console.log("The current queue size is: " + queue1.size());
+console.log(queue1.is_empty());
+console.log(queue1.front());
